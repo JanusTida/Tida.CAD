497ac21b79bb422c9d35ff23fc91ead67cdc2dd8
Janus <Janus@DESKTOP-DQ5FNSC>
2019-12-06 22:12:22 -0800

    Tida.Geometry reference updated to 2.1.0

---
+++ Tida.Canvas.Shell/EditTools/RectangleDiagLinePointsEditToolProvider.cs
@@ -0,0 +1,16 @@
+﻿using Tida.Canvas.Infrastructure.EditTools;
+using Tida.Canvas.Shell.Contracts.EditTools;
+using static Tida.Canvas.Shell.Contracts.EditTools.Constants;
+
+namespace Tida.Canvas.Shell.EditTools {
+    [ExportEditToolProvider(
+        GroupGUID = EditToolGroup_Rectangle,
+        EditToolLanguageKey = Constants.EditToolName_RectangleDiagLinePoints,
+        GUID = EditTool_RectangleDiagLinePoints,
+        IconResource = Constants.EditToolIcon_Rectangle,
+        Order = 4
+    )]
+    public class RectangleDiagLinePointsEditToolProvider : EditToolProviderGenericBase<RectangleDiagLinePointsEditTool>,IEditToolProvider {
+        protected override RectangleDiagLinePointsEditTool OnCreateEditTool() => new RectangleDiagLinePointsEditTool();
+    }
+}
